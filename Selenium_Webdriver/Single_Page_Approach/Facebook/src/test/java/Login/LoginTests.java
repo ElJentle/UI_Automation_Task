f391eff5369d3470cd7ff133d0ea2ca23273b2a0
Jentle Esuong <jen3lity@gmail.com>
2020-12-06 13:54:02 +0100

    add 3 Test suits and update Selenium_Webdriver Task

--- Selenium_Webdriver/Single_Page_Approach/Facebook/src/test/java/Login/LoginTests.java
+++ Selenium_Webdriver/Single_Page_Approach/Facebook/src/test/java/Login/LoginTests.java
@@ -3,6 +3,10 @@ package Login;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
 
@@ -10,10 +14,11 @@ public class LoginTests {
     // import the selenium webDriver
     private WebDriver driver;
     // import chromeDriver
+    @BeforeClass
     public void setup() throws InterruptedException {
         System.setProperty("webdriver.chrome.driver", "resources/chromeDriver.exe");
         driver = new ChromeDriver();
-        // Input project URL = Deydam login URL
+        // Input project URL = facebook login URL
         driver.get("https://www.facebook.com/login");
         // waiting for globally
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
@@ -27,14 +32,54 @@ public class LoginTests {
         driver.findElement(By.id("pass")).sendKeys("09076368799");
         // Click on the sign in button
         driver.findElement(By.xpath("//*[@id=\"loginbutton\"]")).click();
-        Thread.sleep(20000);
-        // close the browser after test
-        driver.quit();
+        Thread.sleep(1000);
     }
+
+    @Test(priority = 0)
+    public void testSuccessfulLogin(){
+        if (driver.getCurrentUrl ().contains ("https://web.facebook.com"))
+            //Pass
+            System.out.println ("The Page URL contains //web.");
+        else
+            //Fail
+            System.out.println ("The Page URL does not contain //web.");
+    }
+
+    @Test(priority = 1)
+    public void testLogout() throws InterruptedException {
+        //click on the account button
+        driver.findElement (By.xpath ("//*[@id=\"mount_0_0\"]/div/div[1]/div[1]/div[2]/div[4]/div[1]/span/div/div[1]/i")).click ();
+        Thread.sleep (5000);
+        //click on the logout button
+        driver.findElement (By.xpath ("//*[@id=\"mount_0_0\"]/div/div[1]/div[1]/div[2]/div[4]/div[2]/div/div[1]/div[1]/div[1]/div/div/div/div/div/div/div/div/div[1]/div/div[3]/div/div[4]/div/div[1]/div[2]/div/div/div")).click ();
+        //printout response based on status
+        if(driver.getCurrentUrl ().contains ("https://www.facebook.com/login"))
+            //Pass
+            System.out.println ("The Login page URL contains /login");
+        else
+            //Fail
+            System.out.println ("The Login URL does not contain /login");
+    }
+
+    @Test(priority = 1)
+    public void testNegativeLogin(){
+        driver.findElement (By.id ("email")).sendKeys ("97887586070");
+        driver.findElement (By.id ("pass")).sendKeys ("09076368799.");
+        driver.findElement (By.name ("login")).click ();
+        String expectedErrorMessage = "The email address or phone number that you've entered doesn't match any account. Sign up for an account.";
+        String actualErrorMessage = driver.findElement(By.xpath ("//*[@id=\"email_container\"]/div[2]")).getText();
+        Assert.assertEquals(actualErrorMessage, expectedErrorMessage);
+    }
+
     // initiate the test run command
     public static void main(String args[]) throws InterruptedException {
         LoginTests test = new LoginTests();
         test.setup();
     }
 
-};
+    @AfterTest
+    public void closeBrowser() throws InterruptedException {
+        Thread.sleep(10000);
+        driver.quit();
+    }
+}
--- Selenium_Webdriver/Single_Page_Approach/Monosnap/src/test/java/Login/LoginTests.java
+++ Selenium_Webdriver/Single_Page_Approach/Monosnap/src/test/java/Login/LoginTests.java
@@ -3,7 +3,11 @@ package Login;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
 
@@ -12,10 +16,11 @@ public class LoginTests {
     private WebDriver driver;
 
     // import chromeDriver
+    @BeforeClass
     public void setup() throws InterruptedException {
         System.setProperty("webdriver.chrome.driver", "resources/chromeDriver.exe");
         driver = new ChromeDriver();
-        // Input project URL = Deydam login URL
+        // Input project URL = monosnap login URL
         driver.get("https://monosnap.com/");
         // waiting for globally
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
@@ -25,20 +30,69 @@ public class LoginTests {
         System.out.println(driver.getTitle());
         // signin navigation
         driver.findElement(By.xpath("//*[@id=\"root\"]/div[2]/div/div[3]/div[1]/div/div[2]/div[3]/span")).click();
-        // Locate username field
-        driver.findElement(By.xpath("//*[@id=\"root\"]/div[2]/div/div[1]/div[1]/div/div[2]/form/div[6]/input[1]")).sendKeys("jentleesuong@yahoo.com");
+        // Locate email field
+        driver.findElement(By.xpath("//*[@id=\"root\"]/div[2]/div/div[1]/div[1]/div/div[2]/form/div[2]/input[1]")).sendKeys("jentleesuong@yahoo.com");
         // Locate password field
-        driver.findElement(By.xpath("//*[@id=\"root\"]/div[2]/div/div[1]/div[1]/div/div[2]/form/div[6]/input[2]")).sendKeys("jentleesuong2020");
+        driver.findElement(By.xpath("//*[@id=\"root\"]/div[2]/div/div[1]/div[1]/div/div[2]/form/div[2]/input[2]")).sendKeys("jentleesuong2020");
         // Click on the sign in button
         driver.findElement(By.xpath("//*[@id=\"root\"]/div[2]/div/div[1]/div[1]/div/div[2]/form/button")).click();
-        Thread.sleep(20000);
-        // close the browser after test
-        driver.quit();
+
+        //*[@id="store"]/div[4]/div/div[1]/div/div[3]/div[3]
+        //*[@id="store"]/div[4]/div/div[1]/div/div[1]/div[2]
+
+        Thread.sleep(2000);
     }
-    // initiate the test run command
+    @Test(priority = 0)
+    public void testSuccessfulLogin(){
+        if (driver.getCurrentUrl ().contains ("https://monosnap.com/list/5fc3b45c8a2e603563a86ae1"))
+            //Pass
+            System.out.println ("The Page URL contains /list/5fc3b45c8a2e603563a86ae1");
+        else
+            //Fail
+            System.out.println ("The Page URL does not contain /list/5fc3b45c8a2e603563a86ae1.");
+    }
+
+    @Test(priority = 1)
+    public void testLogout() throws InterruptedException {
+        //click on the setting button
+        driver.findElement (By.xpath ("//*[@id=\"store\"]/div[4]/div/div[1]/div/div[3]/div[3]")).click ();
+        Thread.sleep (5000);
+        //click on the logout button
+        driver.findElement (By.xpath ("//*[@id=\"store\"]/div[4]/div/div[1]/div/div[1]/div[2]")).click ();
+        //printout response based on status
+        if(driver.getCurrentUrl ().contains ("https://monosnap.com"))
+            //Pass
+            System.out.println ("The Login page URL contains /signin");
+        else
+            //Fail
+            System.out.println ("The Login URL does not contain /signin");
+        Thread.sleep (4000);
+    }
+
+    @Test(priority = 1)
+    public void testNegativeLogin(){
+        driver.findElement(By.xpath("//*[@id=\"root\"]/div[2]/div/div[3]/div[1]/div/div[2]/div[3]/span")).click();
+        // Locate email field
+        driver.findElement(By.xpath("//*[@id=\"root\"]/div[2]/div/div[1]/div[1]/div/div[2]/form/div[2]/input[1]")).sendKeys("jentleesuong@gmail.com");
+        // Locate password field
+        driver.findElement(By.xpath("//*[@id=\"root\"]/div[2]/div/div[1]/div[1]/div/div[2]/form/div[2]/input[2]")).sendKeys("jentleesuong2020");
+        // Click on the sign in button
+        driver.findElement(By.xpath("//*[@id=\"root\"]/div[2]/div/div[1]/div[1]/div/div[2]/form/button")).click();
+        String expectedErrorMessage = "Wrong email or password\n" +
+                "Please check if you've typed your email and password correctly.";
+        String actualErrorMessage = driver.findElement(By.xpath ("//*[@id=\"root\"]/div[1]/div/div/div/div")).getText();
+        Assert.assertEquals(actualErrorMessage, expectedErrorMessage);
+    }
+
+            // initiate the test run command
     public static void main(String args[]) throws InterruptedException {
         LoginTests test = new LoginTests();
         test.setup();
     }
+    @AfterTest
+    public void closeBrowser() throws InterruptedException {
+        Thread.sleep(10000);
+        driver.quit();
+    }
 
-};
+}
